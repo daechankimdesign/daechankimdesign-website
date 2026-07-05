fa0960b92a5cec055fd1a504b14598efbcb2045b
daechankimdesign <daechankim.design@gmail.com>
2026-07-05 14:17:15 -0400

    images

--- .next.nosync/dev/static/chunks/src_app_[locale]_project_page_tsx_0awhgb3._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1iaowux._.js","static/chunks/src_components_07797gd._.js"],
-    source: "dynamic"
-});
